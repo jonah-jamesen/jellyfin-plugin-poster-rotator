51506f9873c373d6d0a0c96af727ae39b8bf080d
Jonah Jamesen <jonahjamesen@gmail.com>
2025-08-15 11:39:26 -0500

    Update Configuration.cs

--- src/Jellyfin.Plugin.PosterRotator/Configuration.cs
+++ src/Jellyfin.Plugin.PosterRotator/Configuration.cs
@@ -1,18 +1,18 @@
-using MediaBrowser.Model.Plugins;
-using System.Collections.Generic;
+using System;
 
-namespace Jellyfin.Plugin.PosterRotator
+public class Configuration : BasePluginConfiguration
 {
-    public class Configuration : BasePluginConfiguration
-    {
-        public string ServerUrl { get; set; } = "http://10.0.0.112:8096";
-        public string ApiKey { get; set; } = "2a45d59c5b6d40268d1b87e4092b2a2a";
-        public List<string> Libraries { get; set; } = new();
-        public int PoolSize { get; set; } = 5;
-        public bool SequentialRotation { get; set; } = true;
-        public bool SaveNextToMedia { get; set; } = true;
-        public bool LockImagesAfterFill { get; set; } = false;
-        public bool DryRun { get; set; } = false;
-        public List<string> ExtraPosterPatterns { get; set; } = new();
-    }
+    public string ServerUrl { get; set; } =
+        Environment.GetEnvironmentVariable("POSTERROTATOR_SERVER_URL") ?? "http://localhost:8096";
+
+    public string ApiKey { get; set; } =
+        Environment.GetEnvironmentVariable("POSTERROTATOR_API_KEY") ?? "";
+
+    public List<string> Libraries { get; set; } = new();
+    public int PoolSize { get; set; } = 5;
+    public bool SequentialRotation { get; set; } = true;
+    public bool SaveNextToMedia { get; set; } = true;
+    public bool LockImagesAfterFill { get; set; } = false;
+    public bool DryRun { get; set; } = false;
+    public List<string> ExtraPosterPatterns { get; set; } = new();
 }
