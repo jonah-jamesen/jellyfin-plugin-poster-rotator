00ec5ca9500f1632b3ed5a2ed417435bd79d7847
Jonah Jamesen <jonahjamesen@gmail.com>
2025-08-17 10:39:16 -0500

    Update Plugin.cs

--- src/Jellyfin.Plugin.PosterRotator/Plugin.cs
+++ src/Jellyfin.Plugin.PosterRotator/Plugin.cs
@@ -1,37 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
-using System;
-using System.Collections.Generic;
 
-namespace Jellyfin.Plugin.PosterRotator
+namespace Jellyfin.Plugin.PosterRotator;
+
+public class Plugin : BasePlugin<Configuration>, IHasWebPages
 {
-    public class Plugin : BasePlugin<Configuration>, IHasWebPages
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
     {
-        // Satisfy nullable analyzer and allow static access in tasks
-        public static Plugin Instance { get; private set; } = null!;
-
-        public override string Name => "Poster Rotator";
-        public override Guid Id => Guid.Parse("7f6eea8b-0e9c-4cbd-9d2a-31f9a37ce2b7");
-
-        public Plugin(IApplicationPaths paths, IXmlSerializer serializer) : base(paths, serializer)
-        {
-            Instance = this;
-        }
-
-        // Tell Jellyfin where our settings page lives
-        public IEnumerable<PluginPageInfo> GetPages()
-        {
-            // The resource path is "<Namespace>.Web.config.html"
-            return new[]
-            {
-                new PluginPageInfo
-                {
-                    Name = "posterrotator", // used in the settings URL
-                    EmbeddedResourcePath = GetType().Namespace + ".Web.config.html"
-                }
-            };
-        }
+        Instance = this;
     }
+
+    public static Plugin? Instance { get; private set; }
+
+    public override string Name => "Poster Rotator"; 
+    public override Guid Id => Guid.Parse("7f6eea8b-0e9c-4cbd-9d2a-31f9a37ce2b7");
+
+    public IEnumerable<PluginPageInfo> GetPages() => new[]
+    {
+        new PluginPageInfo
+        {
+            Name = "Poster Rotator",
+            EmbeddedResourcePath = "Jellyfin.Plugin.PosterRotator.Configuration.config.html"
+        }
+    };
+
 }
