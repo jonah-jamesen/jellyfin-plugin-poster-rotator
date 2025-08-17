778569fb5dc9eaf24da785b60ef5cba4c9530b33
Jonah Jamesen <jonahjamesen@gmail.com>
2025-08-17 10:38:41 -0500

    Update Configuration.cs

--- src/Jellyfin.Plugin.PosterRotator/Configuration.cs
+++ src/Jellyfin.Plugin.PosterRotator/Configuration.cs
@@ -1,18 +1,16 @@
-using System;
+using MediaBrowser.Model.Plugins;
+using System.Collections.Generic;
+
+namespace Jellyfin.Plugin.PosterRotator;
 
 public class Configuration : BasePluginConfiguration
 {
-    public string ServerUrl { get; set; } =
-        Environment.GetEnvironmentVariable("POSTERROTATOR_SERVER_URL") ?? "http://localhost:8096";
-
-    public string ApiKey { get; set; } =
-        Environment.GetEnvironmentVariable("POSTERROTATOR_API_KEY") ?? "";
-
     public List<string> Libraries { get; set; } = new();
-    public int PoolSize { get; set; } = 5;
+    public int  PoolSize { get; set; } = 5;
     public bool SequentialRotation { get; set; } = true;
     public bool SaveNextToMedia { get; set; } = true;
     public bool LockImagesAfterFill { get; set; } = false;
     public bool DryRun { get; set; } = false;
     public List<string> ExtraPosterPatterns { get; set; } = new();
+    public int MinHoursBetweenSwitches { get; set; } = 23;
 }
